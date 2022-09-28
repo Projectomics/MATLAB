2bce2418c90c8b591e4e48db12ef265dcbc654a3
Diek Wheeler <drdiek@mac.com>
2022-09-28 10:35:34 -0400

    Updated lib/shuffle_matrix() to improve the shuffling algorithm and add a text-based completion tracker

--- lib/shuffle_matrix.m
+++ lib/shuffle_matrix.m
@@ -1,19 +1,30 @@
-function shuffleMatrix = shuffle_matrix(shuffleMatrix, nShuffles)
+function shuffleMatrix = shuffle_matrix(rawMatrix, nShuffles)
 
+    % Shuffle binary matrix
+    shuffleMatrix = rawMatrix;
     [nRows, nCols] = size(shuffleMatrix);        
-    count=0;
+    count = 1;
 
-    while count < nShuffles
+    reverseStr = '';
+    oneTenth = nShuffles / 10;
+    fprintf('\n');
+
+    while (count <= nShuffles)
+
+        if (mod(count,oneTenth) == 0)
+            percentDone = 100 * count / nShuffles;
+            msg = sprintf('Shuffling percent done: %3.0f', percentDone);
+            fprintf([reverseStr, msg]);
+            reverseStr = repmat(sprintf('\b'), 1, length(msg));
+        end
 
         % Pick two random, distinct rows to swap values, and determine
         % which row is the top and bottom row
         randRow1 = randi(nRows);
         randRow2 = randRow1;
-
         while (randRow1 == randRow2)
            randRow2 = randi(nRows); 
         end
-
         if (randRow1 < randRow2)
             bottomRow = randRow2;
             topRow = randRow1;
@@ -26,11 +37,9 @@ function shuffleMatrix = shuffle_matrix(shuffleMatrix, nShuffles)
         % which col is the left and right col
         randCol1 = randi(nCols);
         randCol2 = randCol1;
-
         while (randCol1 == randCol2)
-           randCol2 = randi(nCols);
+           randCol2 = randi(nCols); 
         end
-
         if (randCol1 < randCol2)
             leftColumn = randCol1;
             rightColumn = randCol2;
@@ -47,65 +56,54 @@ function shuffleMatrix = shuffle_matrix(shuffleMatrix, nShuffles)
         topRight = shuffleMatrix(topRow, rightColumn);
         bottomLeft = shuffleMatrix(bottomRow, leftColumn);
         bottomRight = shuffleMatrix(bottomRow, rightColumn);
-
-        % From the cells along the diagonal that will be losing
-        % counts, determine what the max value that can be swapped is 
-        % This has to be the smaller value in the diagonal cells, 
-        % because the same amount will be lost from both cells and we
-        % want to avoid negative numbers in cells
-        if (topLeft < bottomRight)
-            maxBound = topLeft;
-        else
-            maxBound = bottomRight;
-        end
         
-        % If the max value that can be swapped is 0, see if the cells
-        % along the other diagonal will yield a different max value if
-        % that diagonal became the one that will lose counts
-        % This can happen only if both cells along the other diagonal
-        % are not 0; if this is the case, then change the max value
-        % that can be swapped to the new value
-        if (maxBound == 0)
+        % Find the maximal values along the diagonals to be swapped
+        minTopLeftBottomRight = min([topLeft bottomRight]);
+        minTopRightBottomLeft = min([topRight bottomLeft]);
 
-            if(topRight < bottomLeft)
-                maxBound = topRight;
-            else
-                maxBound = bottomLeft;
+        % Randomly select a swapping direction
+        coin = randi(2) - 1;
+
+        if coin % prioritize the topLeft-bottomRight diagonal
+            if minTopLeftBottomRight % if non-zero, perform the swap
+                value = randi(minTopLeftBottomRight);
+                shuffleMatrix(topRow, leftColumn) = shuffleMatrix(topRow, leftColumn)-value;
+                shuffleMatrix(bottomRow, leftColumn) = shuffleMatrix(bottomRow, leftColumn)+value;
+                shuffleMatrix(topRow, rightColumn) = shuffleMatrix(topRow, rightColumn)+value;
+                shuffleMatrix(bottomRow, rightColumn) = shuffleMatrix(bottomRow, rightColumn)-value;
+                count = count + 1;
+            else % try the other diagonal
+                if minTopRightBottomLeft % if non-zero, perform the swap
+                    value = randi(minTopRightBottomLeft);
+                    shuffleMatrix(topRow, leftColumn) = shuffleMatrix(topRow, leftColumn)+value;
+                    shuffleMatrix(bottomRow, leftColumn) = shuffleMatrix(bottomRow, leftColumn)-value;
+                    shuffleMatrix(topRow, rightColumn) = shuffleMatrix(topRow, rightColumn)-value;
+                    shuffleMatrix(bottomRow, rightColumn) = shuffleMatrix(bottomRow, rightColumn)+value;
+                    count = count + 1;
+                end                
             end
-
-    	    if maxBound % not equal to zero
-
-                % Choose a random value to be swapped between cells <=
-                % the max value that can be swapped
-                value = randi(maxBound);
-                
-                % Swap that value across 4 cells and increment number
-                % of shuffles
+        else % prioritize the topRight-bottomLeft diagonal
+            if minTopRightBottomLeft % if non-zero, perform the swap
+                value = randi(minTopRightBottomLeft);
                 shuffleMatrix(topRow, leftColumn) = shuffleMatrix(topRow, leftColumn)+value;
                 shuffleMatrix(bottomRow, leftColumn) = shuffleMatrix(bottomRow, leftColumn)-value;
                 shuffleMatrix(topRow, rightColumn) = shuffleMatrix(topRow, rightColumn)-value;
-                shuffleMatrix(bottomRow, rightColumn) = shuffleMatrix(bottomRow, rightColumn)+value;               
+                shuffleMatrix(bottomRow, rightColumn) = shuffleMatrix(bottomRow, rightColumn)+value;
                 count = count + 1;
-
-            end % if maxBound
-
-        else
-
-            % Choose a random value to be swapped between cells <=
-            % the max value that can be swapped
-            value = randi(maxBound);
-            
-            % Swap that value across 4 cells and increment number
-            % of shuffles
-            shuffleMatrix(topRow, leftColumn) = shuffleMatrix(topRow, leftColumn)-value;
-            shuffleMatrix(bottomRow, leftColumn) = shuffleMatrix(bottomRow, leftColumn)+value;
-            shuffleMatrix(topRow, rightColumn) = shuffleMatrix(topRow, rightColumn)+value;
-            shuffleMatrix(bottomRow, rightColumn) = shuffleMatrix(bottomRow, rightColumn)-value;   
-
-            count = count+1;
-            
-        end % if (maxBound == 0)
+            else % try the other diagonal
+                if minTopLeftBottomRight % if non-zero, perform the swap
+                    value = randi(minTopLeftBottomRight);
+                    shuffleMatrix(topRow, leftColumn) = shuffleMatrix(topRow, leftColumn)-value;
+                    shuffleMatrix(bottomRow, leftColumn) = shuffleMatrix(bottomRow, leftColumn)+value;
+                    shuffleMatrix(topRow, rightColumn) = shuffleMatrix(topRow, rightColumn)+value;
+                    shuffleMatrix(bottomRow, rightColumn) = shuffleMatrix(bottomRow, rightColumn)-value;
+                    count = count + 1;
+                end
+            end
+        end % coin
 
     end % while count
+
+    fprintf('\n');
 
 end % shuffle_matrix()
