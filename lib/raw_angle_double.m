a32457e36f14ddb588754c1d72a858c2427f05cc
Diek Wheeler <drdiek@mac.com>
2022-09-28 10:32:39 -0400

    Added text-based completion tracker to lib/raw_angle_double()

--- lib/raw_angle_double.m
+++ lib/raw_angle_double.m
@@ -5,6 +5,10 @@ function doublesData = raw_angle_double(data)
     
     nRows = size(data, 1);
     
+    reverseStr = '';
+    nComparisons = nRows*(nRows-1)/2;
+    oneTenth = nComparisons / 10;
+
     c = 0;
     for i = 1:nRows
         for ii = i+1:nRows
@@ -17,7 +21,16 @@ function doublesData = raw_angle_double(data)
             angle = round(angle);
             c = c + 1;
             doublesData(c, :) = [i ii angle];
+
+            if (mod(c,oneTenth) == 0)
+                percentDone = 100 * c / nComparisons;
+                msg = sprintf('Computing angles percent done: %3.0f', percentDone);
+                fprintf([reverseStr, msg]);
+                reverseStr = repmat(sprintf('\b'), 1, length(msg));
+            end
         end % ii
     end % i
+
+    fprintf('\n');
 
 end % combine_doubles()
